71d9ab57347f57f244439ad3df3e0865495d0ab5
Elky Bachtiar <139325226+elky-bachtiar@users.noreply.github.com>
2025-06-13 23:30:55 +0200

    Deleted Footer.tsx

--- src/components/Logo.tsx
+++
@@ -1,9 +0,0 @@
-import { Link } from 'react-router-dom';
-
-export function Logo({ className = '' }: { className?: string }) {
-  return (
-    <Link to="/" className={`block ${className}`}>
-      <img src="/logo.png" alt="Shop2Give" className="h-10" />
-    </Link>
-  );
-}
